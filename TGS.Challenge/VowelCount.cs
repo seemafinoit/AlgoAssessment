31632dabe97d10b294ed5075f9318d7706c7f3d2
Seema Gupta <seema@finoit.co.in>
2021-02-24 14:35:04 +0530

    Challenge 3 - VowelCount Program implementation

--- TGS.Challenge/VowelCount.cs
+++ TGS.Challenge/VowelCount.cs
@@ -16,7 +16,29 @@ namespace TGS.Challenge
     {
         public int Count(string value)
         {
-            return -1;
+            //Check if value is null or empty then throw ArgumentException
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException();
+
+            int i, len, vowel;
+
+            vowel = 0;
+            len = value.Length; // length of value
+
+            //convert the value in lower case
+            value = value.ToLower();
+
+            //loop each character
+            for (i = 0; i < len; i++)
+            {
+                // check if character is equal to 'a', 'e', 'i', 'o', 'u'
+                if (value[i] == 'a' || value[i] == 'e' || value[i] == 'i' || value[i] == 'o' || value[i] == 'u')
+                {
+                    vowel++;
+                }
+            }
+
+            return vowel;
         }
     }
 }
